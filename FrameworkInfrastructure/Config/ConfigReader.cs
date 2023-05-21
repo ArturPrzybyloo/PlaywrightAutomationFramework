9805695b178e45a8ff2ce447e44a584198344e8f
Artur Przybyło <a.przybyloo@gmail.com>
2023-05-21 20:58:42 +0200

    Changed config path to use AppDomain

--- FrameworkInfrastructure/Config/ConfigReader.cs
+++ FrameworkInfrastructure/Config/ConfigReader.cs
@@ -5,18 +5,14 @@ namespace FrameworkInfrastructure.Config
     public class ConfigReader
     {
         private readonly Config _config;
-        //private readonly string _jsonFilePath = "C:\\Users\\HP\\Desktop\\Projects\\PlaywrightAutomationFramework\\EaFramework\\EaFramework\\Config\\config.json";
 
         public ConfigReader()
         {
             // Get the base directory of your application
-            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            var baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
 
             // Specify the relative path to the config file
-            string relativePath = Path.Combine("Config", "config.json");
-
-            // Combine the base directory and relative path to get the complete file path
-            string filePath = Path.Combine(baseDirectory, relativePath);
+            var filePath = Path.Combine(baseDirectory, "config.json");
 
             var configJson = File.ReadAllText(filePath);
             _config = JsonConvert.DeserializeObject<Config>(configJson);
